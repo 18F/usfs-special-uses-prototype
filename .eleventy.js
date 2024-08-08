2eb59cf6f30bbf3922cbd8986874759fdd6f99d8
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2024-08-08 14:25:38 -0500

    Add pa11y tests

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
+const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(sitemap, {
+    sitemap: {
+      hostname: "http://localhost:8080",
+    },
+  });
   return {
     dir: {
       input: "src",
